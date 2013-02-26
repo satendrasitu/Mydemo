19308c6006f3786274988b82ae1851d917fe1fa3
satendrasitu <satendrasitu@gmail.com>
2013-02-26 11:04:45 +0530

    saved

---
+++ Mydemo/Mydemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Mydemo
+//
+//  Created by Satendra Singh on 26/02/13.
+//  Copyright (c) 2013 Satendra Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
